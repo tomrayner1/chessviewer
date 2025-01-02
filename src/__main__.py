a00a41fd96845d6dd2ee3932787679ed34fb73ac
tomrayner1 <52078213+tomrayner1@users.noreply.github.com>
2025-01-02 18:14:01 +0000

    experimenting with chessboard calibration

--- src/__main__.py
+++ src/__main__.py
@@ -7,9 +7,22 @@ def main():
   screen = pyautogui.screenshot()
 
   frame = numpy.array(screen)
-  frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-  cv2.imshow("Current desktop", frame)
+  # For demo purposes
+  frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+  pattern_size = (7, 7)
+  ret, corners = cv2.findChessboardCorners(gray, pattern_size)
+
+  if ret:
+    print("Chessboard found.")
+
+    cv2.drawChessboardCorners(frame, pattern_size, corners, ret)
+  else:
+    print("Couldn't find a chessboard.")
+
+  cv2.imshow("frame", frame)
   cv2.waitKey(0)
 
   return
